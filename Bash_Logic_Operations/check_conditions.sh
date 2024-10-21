bb96046c4bea430cd3ca39dc4f23e4824b196c68
talatunyande <nyandetalatu@gmail.com>
2024-10-21 12:39:27 +0000

    Tech4girls_Backend

---
+++ Bash_Logic_Operations/check_conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+Firstnumber=$1
+Secondnumber=$2
+if  [  "$1"  -gt 10  -a "$2" -gt 10  ] then
+echo "numbers are greater than 10 "
+else
+if [  "$1" "$"  -gt 10  || "$2" -lt 10 ] then 
+echo "Atleast one number is greter than  10 "
+if [  "$1" "$"  -it 10  && "$2" -lt 10 ] then 
+echo "Atleast one number is greter than  10 "
---
+++ Bash_Logic_Operations/countdown.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# this script is to assign numbers greater 
+Firstnumber=$1
+Secondnumber=$2
+echo "
