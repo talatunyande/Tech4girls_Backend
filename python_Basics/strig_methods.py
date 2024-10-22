e398ee30cc4a7c98e9e5018e03cfbb686cf804f4
talatunyande <nyandetalatu@gmail.com>
2024-10-22 10:12:23 +0000

    Tech4girls_Backend

---
+++ python_Basics/strig_methods.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# uppercase words
+About ='My name is Talatu Nyande'
+print (About.upper())
+
+# lowercase
+About ='My name is Talatu Nyande'
+print (About.lower())
+
+# Replace
+About ='My name is Talatu Nyande'
+print (About.replace ("Talatu","Rahana") )
+
+#Join
+ About ='My name is Talatu Nyande'
+print (About.join ())
+
