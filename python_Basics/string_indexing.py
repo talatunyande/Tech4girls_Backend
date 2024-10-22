b92bd95a6901495285cde5427bba74cb4ba99638
talatunyande <nyandetalatu@gmail.com>
2024-10-22 16:37:57 +0000

    Tech4girls_Backend

---
+++ python_Basics/string_indexing.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+sentence = 'python is powerful and easy to learn'
+print (sentence[0:6])
+print  (sentence[31:36])
+print (sentence [10:19])
+
+
+              
+
+
+
+
+       
+       
