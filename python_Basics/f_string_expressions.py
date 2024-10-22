aa1e918c061612f478482c6ccab822b12a447281
talatunyande <nyandetalatu@gmail.com>
2024-10-22 10:27:04 +0000

    Tech4girls_Backend

---
+++ python_Basics/f_string_expressions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# f_string
+a=10
+print(a)
+b=20
+print(b)
+sum=a+b
+print(sum)
+product=a*b
+print(product) 
+difference=a-b
+print(difference)
+print (f"the summation of a and b is {sum},the product of a and b is {product} and the difference of a and b is {difference}")
