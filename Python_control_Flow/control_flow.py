1ea7536cf37833a7b29cb3e3655a2075d8d214ba
talatunyande <nyandetalatu@gmail.com>
2024-10-28 03:00:06 +0000

    Tech4girls_Backend

---
+++ Python_control_Flow/control_flow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+if condition
+"""
+age =int(input("please enter your age :"))
+if age < 18 :
+    print("you are a minor")
+elif 18 <=age <=64 :
+    print("you are an adult")
+else :
+    print ("you are a senior")
+
