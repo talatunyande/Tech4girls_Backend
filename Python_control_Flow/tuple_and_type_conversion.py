f3c858bd9ac90e77d11e5b33b096a3324c71047a
talatunyande <nyandetalatu@gmail.com>
2024-10-28 02:47:43 +0000

    Tech4girls_Backend

---
+++ Python_control_Flow/tuple_and_type_conversion.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+element_tuple=(1,2,3,4,5,6)
+print(element_tuple)
+print(element_tuple[0])
+print(element_tuple[5])
+#demonstrating the use of count and index
+print(element_tuple.count(5))
+#tuple demonstration
+print(element_tuple.index(1))
+#converting from integer to float ,float to integer and string to integer
+x=10
+print(float(x))
+y=10.0
+print(int(y))
+z='10'
+print(int(z))
+#joining list of strings in to single strings
+dollar='4'
+cedis='5'
+join=dollar.join(cedis)
+print(join)
+
+
+
+
+
+
+
