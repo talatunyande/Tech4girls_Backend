e8ff547e0bbc404584958924acd7f78600f40f22
talatunyande <nyandetalatu@gmail.com>
2024-11-25 15:08:51 +0000

    Many to many relationships

--- SQL_Assignment/question1.sql
+++ SQL_Assignment/question1.sql
@@ -1,22 +1,25 @@
 /* Creating a database called Tech4Girls_DB */
 CREATE DATABASE IF NOT EXISTS Tech4Girls_DB;
+
 SHOW DATABASES;
+
 USE Tech4Girls_DB ;
 
 /* creating a table called Users*/
-CREATE TABLE IF NOT EXISTS Users(
-
-    id AUTO_INCREMENT PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS Users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100)  NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-INSERT INTO Users (id ,username ,email,created_at)
+
+INSERT INTO Users (username ,email)
 VALUES
-('ama','amaple@example.com','2024-11-01 10:30.00'), --username field
-('Abens','abena@example.com','2024-11-02 12:00:00'), --email field
-('adjoa','adjoa@example.com','2024-11-03 14:15:00'); -- created_at field
+('ama','amaple@example.com'), -- username field
+('Abens','abena@example.com'), -- email field
+('adjoa','adjoa@example.com'); -- created_at field
+
 SHOW TABLES;
 
-SELECT* FROM Users;
+SELECT * FROM Users;
 
